ac9cae4623c8ac52fedf81f49032f956dae59e80
crypto0sss <mjflow99@gmail.com>
2024-10-08 20:03:17 +0300

    Delete lr1 directory

--- lr1/Meat.java
+++
@@ -1,9 +0,0 @@
-package FoodLib;
-
-import java.util.Map;
-
-public class Meat extends Food {
-    public Meat(String name, boolean edible, Map<String, Double> nutritionalValue) {
-        super(edible, nutritionalValue, name);
-    }
-}
